e97d62946e701c42cf4b1a45c210dd57a91ddf11
AhmedouSalem <ahmedou8salem@gmail.com>
2026-05-06 22:03:37 +0200

    create project + add docker-compose file

---
+++ src/main/java/com/example/discoprep/DiscoPrepApplication.java
@@ -0,0 +1,13 @@
+package com.example.discoprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoPrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoPrepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/discoprep/DiscoPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discoprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoPrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
